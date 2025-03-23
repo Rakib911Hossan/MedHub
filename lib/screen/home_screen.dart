b87a890780c883718d6a161ccbc4376ccedd0d1c
Rakib911Hossan <rakib911hossan@gmail.com>
2025-03-24 03:57:02 +0600

    Feat: this adds updete profile of user

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -43,7 +43,7 @@ class HomeScreen extends StatelessWidget {
               title: const Text('Profile'),
               onTap: () async {
                 // await FirebaseAuth.instance.signOut();
-                Navigator.pushReplacement(
+                Navigator.push(
                   context,
                   MaterialPageRoute(
                     builder: (context) => const ProfileScreen(),
